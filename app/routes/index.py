ed0469635b8a4267c722708352a1898ee05da0b4
robertovicario <robertovicario.com@gmail.com>
2025-08-14 15:08:17 +0200

    Deployed the app

--- app/routes/index.py
+++ app/routes/index.py
@@ -6,6 +6,21 @@ index_bp = Blueprint('index', __name__)
 
 # -------------------------
 
+conversation = [
+    {
+        'id': 1,
+        'role': 'user',
+        'content': 'Ciao!',
+        'timestamp': '2025-08-14T10:00:00Z'
+    },
+    {
+        'id': 2,
+        'role': 'bot',
+        'content': 'Ciao, come stai?',
+        'timestamp': '2025-08-14T10:00:05Z'
+    }
+]
+
 @index_bp.route('/')
 def page():
-    return render_template('pages/home.html')
+    return render_template('pages/home.html', conversation=conversation)
