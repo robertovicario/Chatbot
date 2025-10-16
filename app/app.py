39cb3a418ae4b498fbfcc385ec9e890d49bb80ed
Roberto Vicario <119845903+robertovicario@users.noreply.github.com>
2025-10-16 11:24:20 +0200

    Refactor app.py by removing session route and secret key
    Removed session handling route and secret key generation.

--- app/app.py
+++ app/app.py
@@ -1,5 +1,4 @@
-from flask import Flask, jsonify, session
-import secrets
+from flask import Flask
 
 from routes.chat import chat_bp
 from routes.fallback import fallback_bp
@@ -8,7 +7,6 @@ from routes.index import index_bp
 # -------------------------
 
 app = Flask(__name__)
-app.secret_key = secrets.token_hex(32)
 
 # -------------------------
 
@@ -20,10 +18,6 @@ app.register_blueprint(fallback_bp)
 app.register_blueprint(index_bp)
 
 # -------------------------
-
-@app.route('/session')
-def app_session():
-    return jsonify(dict(session))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=7860)
